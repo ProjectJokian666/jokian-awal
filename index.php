383be0f0c5bc0589185a821c6c7deac5ecbf6a64
ProjectJokian666 <ProjectJokian666@gmail.com>
2022-09-20 01:16:37 +0700

    nambah isi

---
+++ index.php
@@ -0,0 +1,16 @@
+git init
+
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
+
+git remote add origin https://github.com/ProjectJokian666/jokian-awal.git
+
+git branch -M main
+
+
+//diulang jika ingin up tugas
+git add .
+
+git commit -m "first commit"
+
+git push -u origin main
